23d0fbc50992339e4f487db72523a4ef62d8b874
Ashley Roach <aroach@gmail.com>
2015-05-31 17:55:58 -0600

    update output

--- pm2apitest.js
+++ pm2apitest.js
@@ -1,41 +1,50 @@
 
 var pm2 = require('pm2');
 
-pm2.connect(function(err) {
 
-	var processes = [];
-	var health = {};
-	var overall_health = 'online';
+var pm2_health = function() {
 
-	pm2.list(function(err, process_list) {
+	pm2.connect(function(err) {
 
-		if (err) console.log(err);
+		var processes = [];
+		var health = {};
+		var overall_health = 'online';
 
-		process_list.forEach(function(element, index) {
-			//console.log(element);
-			processes.push({
-				env: element.pm2_env.NODE_ENV,
-				seed: element.pm2_env.SEED,
-				name: element.pm2_env.name,
-				pm_id: element.pm2_env.pm_id,
-				status: element.pm2_env.status
+		pm2.list(function(err, process_list) {
+
+			if (err) console.log(err);
+
+			process_list.forEach(function(element, index) {
+				//console.log(element);
+				processes.push({
+					env: element.pm2_env.NODE_ENV,
+					seed: element.pm2_env.SEED,
+					name: element.pm2_env.name,
+					pm_id: element.pm2_env.pm_id,
+					status: element.pm2_env.status
+				});
+
+				if (!element.pm2_env.status === 'online') {
+					overall_health = 'failing';
+				}
+
 			});
 
-			if (!element.pm2_env.status === 'online') {
-				overall_health = 'failing';
+			health = {
+				overall: overall_health,
+				proc_list: processes
 			}
+
+			//console.log(health);
+
+			return health;
+
+			pm2.disconnect(function() { process.exit(0) });
 
 		});
 
-		health = {
-			overall: overall_health,
-			proc_list: processes
-		}
-
-		console.log(health);
-
-		pm2.disconnect(function() { process.exit(0) });
-
 	});
 
-});
+};
+
+console.log(pm2_health);
