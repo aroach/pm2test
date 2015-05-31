890c16a060d892350840410194afbf05b36a1693
Ashley Roach <aroach@gmail.com>
2015-05-31 17:43:03 -0600

    update output

--- pm2apitest.js
+++ pm2apitest.js
@@ -2,17 +2,27 @@
 var pm2 = require('pm2');
 
 pm2.connect(function(err) {
-	pm2.describe( 0, function(err, ret) {
+
+	var processes = [];
+
+	pm2.list(function(err, process_list) {
+
 		if (err) console.log(err);
-		console.log('======= pm2 describe ==========');
-		console.log("ret:");
-		console.log(ret[0]);
-		console.log(ret[0].pm2_env.status);
-		//console.log(ret[0].pm2_env.versioning.revision);
+
+		process_list.forEach(function(element, index) {
+			//console.log(element);
+			processes.push({
+				env: element.pm2_env.NODE_ENV,
+				seed: element.pm2_env.SEED,
+				name: element.pm2_env.name,
+				pm_id: element.pm2_env.pm_id,
+				status: element.pm2_env.status
+			});
+		});
+
+		console.log(processes);
 
 		pm2.disconnect(function() { process.exit(0) });
-
-		//result = ret;
-
 	});
+
 });
